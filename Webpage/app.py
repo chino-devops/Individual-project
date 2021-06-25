dcb27cc13163cf692c2d88bffb40acee51b28f23
chino-devops <danielnwankwo95@gmail.com>
2021-06-25 14:44:25 +0000

    2nd again

--- Webpage/app.py
+++ Webpage/app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Welcome to the home page'
+    return 'Welcome to the home page, Happy Browsing! :)'
 
 @app.route('/about')
 def about():
-    return 'This is the about page'
+    return 'This is the about page, please bear with us as we are still building!'
 
 if __name__ == "__main__":
     app.run(debug=True)
